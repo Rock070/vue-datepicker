d9ee822f9746fae84dd2f7e7fe5361e4750e9746
Rock070 <a0938130702@gmail.com>
2022-12-31 01:54:04 +0800

    feat(helpers): getTimeLocal, getNavigatorLanguage

--- src/helpers/getNavigatorLocale.ts
+++ src/helpers/getNavigatorLocale.ts
@@ -1,8 +1,8 @@
-const getNavigatorLanguage = () => {
+const getNavigatorLocale = () => {
   if (navigator.languages && navigator.languages.length) {
     for (const i of navigator.languages) if (i) return i;
   }
   return navigator.language || 'en';
 };
 
-export default getNavigatorLanguage;
+export default getNavigatorLocale;
